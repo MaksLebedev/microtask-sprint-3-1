a6045f2b42cb48887b85ff7fd27b870034f234c1
Maksim Lebedev <monk527@yandex.ru>
2024-11-08 10:38:48 +0500

    add react-router-dom and add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import styles from "./components/Site.module.css";
-
+import { PageOne } from "./components/pages/PageOne";
+import { PageTwo } from "./components/pages/PageTwo";
+import { PageThree } from "./components/pages/PageThree";
+import { Route, Routes } from "react-router-dom";
+import { Error404 } from "./components/pages/Error404";
 
 function App() {
-    return (
-        <div>
-            <div className={styles.header}><h1>HEADER</h1></div>
-            <div className={styles.body}>
-                <div className={styles.nav}>
-                    Здесь будет навигация
-                </div>
-                <div className={styles.content}>
-                    Здесь будут кроссовки
-                </div>
-            </div>
-            <div className={styles.footer}>abibas 2023</div>
+  return (
+    <div>
+      <div className={styles.header}>
+        <h1>HEADER</h1>
+      </div>
+      <div className={styles.body}>
+        <div className={styles.nav}>Здесь будет навигация</div>
+        <div className={styles.content}>
+          <Routes>
+            <Route element={<PageOne />} path="/page1" />
+            <Route element={<PageTwo />} path="/page2" />
+            <Route element={<PageThree />} path="/page3" />
+            <Route element={<Error404 />} path="/*" />
+          </Routes>
         </div>
-    );
+      </div>
+      <div className={styles.footer}>abibas 2023</div>
+    </div>
+  );
 }
-
 
 export default App;
